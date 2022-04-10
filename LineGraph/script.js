bdc8421b3f0a6ff61017d94bb3af2d4c46e47b3f
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2022-04-10 14:15:25 +0900

    Create script.js

---
+++ LineGraph/script.js
@@ -0,0 +1,52 @@
+var myChart = new Chart(document.getElementById("script"), {
+  type: 'line',
+  data: {
+      labels: ['3', '6', '9', '12', '15', '18', '21'],
+      datasets: [{
+          label: '테스트 데이터셋',
+          data: [
+              10,
+              2,
+              3,
+          ],
+          borderColor: "rgba(255, 201, 14, 1)",
+          backgroundColor: "rgba(255, 201, 14, 0.5)",
+          fill: true,
+          lineTension: 0
+      }]
+  },
+  options: {
+      responsive: true,
+      title: {
+          display: true,
+          text: '라인 차트 테스트'
+      },
+      tooltips: {
+          mode: 'index',
+          intersect: false,
+      },
+      hover: {
+          mode: 'nearest',
+          intersect: true
+      },
+      scales: {
+          xAxes: [{
+              display: true,
+              scaleLabel: {
+                  display: true,
+                  labelString: 'Time'
+              }
+          }],
+          yAxes: [{
+              display: true,
+              ticks: {
+                  suggestedMin: 0,
+              },
+              scaleLabel: {
+                  display: true,
+                  labelString: 'Temp'
+              }
+          }]
+      }
+  }
+});
