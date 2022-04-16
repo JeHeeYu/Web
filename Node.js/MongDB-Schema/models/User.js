7e3fd2ef76a55a9da3af00676d5cf58941e1696a
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2022-04-16 14:40:23 +0900

    Create User.js

---
+++ Node.js/MongDB-Schema/models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('module');
+const { Schema } = mongoose;
+const userSchema = new Schema ({
+    email : { 
+        type : String,
+        required : true,
+    },
+    name : String,
+    age : {
+        type : Number,
+        min : 18,
+        max : 50,
+        },
+    },
+    {
+    timestamps : true,
+    }
+);
+
+module.exports = mongoose.model("User" , userSchema);
